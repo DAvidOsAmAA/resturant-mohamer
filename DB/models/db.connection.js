c9c38b3d0f8934b9721284f83be1ab6205900599
Ahmedfci378 <ahmedfci378@gmail.com>
2026-01-26 23:07:04 +0200

    second

--- DB/models/db.connection.js
+++ DB/models/db.connection.js
@@ -1,21 +1,4 @@
 const mongoose = require('mongoose');
-
-<<<<<<< HEAD
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL);
-    console.log('MongoDB Connected');
-  } catch (err) {
-    console.error('MongoDB Connection Failed:', err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
-
-
-
-=======
 module.exports.dbConnection=()=>{
     mongoose.connect(process.env.DB_URL)
         .then(() => console.log('Database connected'))
@@ -24,4 +7,4 @@ module.exports.dbConnection=()=>{
             process.exit(1);
         });
 }
->>>>>>> origin/dev
+
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('../DB/models/db.connection');
-const userRoutes = require('./modules/user module/user.routes');
-
-const app = express();
-
-connectDB();
-
-app.use(express.json());
-
-app.use('/api/auth', userRoutes);
-
-module.exports = app;
-=======
 const { dbConnection } = require("../DB/models/db.connection");
 const app = require('express').Router();
 require('dotenv').config({path :'./.env'})
@@ -24,9 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 dbConnection();
 app.get('/', (req, res) => res.send('Hello World!'));
 
 module.exports = app;
->>>>>>> origin/dev
