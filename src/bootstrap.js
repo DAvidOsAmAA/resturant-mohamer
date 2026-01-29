6cc9ee24cbf826b3e23c2dfcb612d1f7098044e1
ZiadtahaM <fagricastro@gmail.com>
2026-01-29 23:04:07 +0200

    d

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,7 @@
+const express = require('express'); 
 const { dbConnection } = require("../DB/models/db.connection");
-const app = require('express').Router();
-const mealRoutes=require( './modules/meals modules/meal.route')
+const app = express();
+const mealRoutes=require( './modules/mealsmodules/meal.route')
 require('dotenv').config({path :'./.env'})
 
 const port = process.env.PORT || 3000;
@@ -10,8 +11,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+
 dbConnection();
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.use('/meals',mealRoutes)
+app.use('/meals', mealRoutes);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).json({
+        success: false,
+        error: err.message || 'Internal Server Error'
+    });
+});
+app.use((req, res) => {  
+    res.status(404).json({
+        success: false,
+        error: 'Route not found'
+    });
+});
 module.exports = app;
