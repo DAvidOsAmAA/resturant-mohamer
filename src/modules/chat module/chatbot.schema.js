3bb11430ee19886a6895939a1c65209f752c7b85
Umar Yasser <alta2ralgare7@gmail.com>
2026-02-01 09:02:18 +0200

    Schema File added

--- src/modules/chat module/chatbot.schema.js
+++ src/modules/chat module/chatbot.schema.js
@@ -1,1 +1,39 @@
 // Joi Validatation
+import Joi from 'joi'
+import mongoose from 'mongoose'
+
+// Custom ObjectId validator
+const objectId = (value, helpers) => {
+  if (!mongoose.Types.ObjectId.isValid(value)) {
+    return helpers.message('Invalid userId')
+  }
+  return value
+}
+
+export const chatSchema = Joi.object({
+  userId: Joi.string()
+    .required()
+    .custom(objectId)
+    .messages({
+      'any.required': 'User ID is required',
+      'string.empty': 'User ID is required'
+    }),
+
+  history: Joi.array()
+    .items(
+      Joi.object({
+        role: Joi.string()
+          .valid('user', 'assistant', 'system')
+          .required(),
+
+        message: Joi.string()
+          .trim()
+          .min(1)
+          .required()
+      })
+    )
+    .required()
+    .messages({
+      'any.required': 'Conversation is required'
+    })
+})
